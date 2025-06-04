b547793a5f213864ad4f02a8b40b4c2ac77a342d
Paolo Lenin Artigas Chacin <paoloartigas21@gmail.com>
2025-06-03 21:58:42 -0400

    Form reviews

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -9,9 +9,17 @@ class ExperiencesController < ApplicationController
   end
 
   def new
+    @experience = Experience.new
   end
 
   def create
+    @experience = Experience.new(experience_params)
+    @experience.host = current_user
+    if @experience.save
+      redirect_to @experience, notice: 'Experience was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -21,5 +29,11 @@ class ExperiencesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def experience_params
+    params.require(:experience).permit(:category, :title, :location, :description, :rating, :price)
   end
 end
