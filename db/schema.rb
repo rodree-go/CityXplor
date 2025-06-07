b27a442c65456a889304c3063694880685eba859
Rodrigo Flores <floresrodrigodf@gmail.com>
2025-06-07 12:20:30 -0600

    fix: se actualizan los cambios a schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_06_07_155106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
