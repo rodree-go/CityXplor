c70b7ba17a90edfafc497b85707741b186da552d
CristobalNoriega <cristobal.noriegaa@gmail.com>
2025-06-07 15:47:41 -0400

    fix: mejoras a las tablas

--- db/migrate/20250607155106_remove_column_from_bookings.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnFromBookings < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :bookings, :start_time, :time
-    remove_column :bookings, :end_time, :time
-  end
-end
