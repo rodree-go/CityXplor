bfce9adcc2a0715fdbf6baea6216b58e4ae9ed74
Rodrigo Flores <floresrodrigodf@gmail.com>
2025-06-07 13:47:37 -0600

    fix: mejoras a las tablas

--- db/migrate/20250606003955_add_image_url_to_experiences.rb
+++
@@ -1,5 +0,0 @@
-class AddImageUrlToExperiences < ActiveRecord::Migration[7.1]
-  def change
-    add_column :experiences, :img_url, :string
-  end
-end
--- db/migrate/20250606033757_add_availability_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddAvailabilityToBookings < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :bookings, :availability, foreign_key: true
-  end
-end
